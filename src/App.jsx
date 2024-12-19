9ef43b3bd5432e42f534e2b38b20a2c4f3cece0e
emapg <emap04@proton.me>
2024-12-19 19:58:34 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import MarkdownConverter from './MarkdownConverter';
+import About from './About';
+import FAQ from './FAQ';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
       <MarkdownConverter />
+      <div className="container mx-auto p-6 mt-10">
+        <About />
+      </div>
+      <div className="container mx-auto p-6 mt-10">
+        <FAQ />
+      </div>
     </div>
   );
 }
